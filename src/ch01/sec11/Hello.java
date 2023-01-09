21648e17f6671eca2c5170175ec41ba1d5930d72
seo-faper <dswhdcks@gmail.com>
2023-01-09 14:57:09 +0900

    자바 1강

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,12 @@
+package ch01.sec11;
+
+public class Hello
+{
+    /**
+     * @author seo-faper
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
