5ebf84662e694de9db01f90644484f15a370a781
seo-faper <dswhdcks@gmail.com>
2023-01-10 10:19:09 +0900

    예제코드 : IfElseExample.java

--- src/ch04/sec02/IfElseExample.java
+++ src/ch04/sec02/IfElseExample.java
@@ -7,10 +7,10 @@ public class IfElseExample
 
         if(score >= 90){
             System.out.println("점수가 90보다 큽니다.");
-            System.out.println("등급은 A입니다.");
+            System.out.println("등급은 A 입니다.");
         } else {
             System.out.println("점수가 90보다 작습니다.");
-            System.out.println("등급은 B입니다.");
+            System.out.println("등급은 B 입니다.");
         }
     }
 }
