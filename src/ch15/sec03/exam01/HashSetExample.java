29bd78448455028e8f7e77aea06abfe98b8c2552
seo-faper <dswhdcks@gmail.com>
2023-01-10 20:19:16 +0900

    예제코드 : HashSetExample.java

---
+++ src/ch15/sec03/exam01/HashSetExample.java
@@ -0,0 +1,20 @@
+package ch15.sec03.exam01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample
+{
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+
+        set.add("Java");
+        set.add("JDBC");
+        set.add("JSP");
+        set.add("Java"); // 저장 하지 않음
+        set.add("Spring");
+
+        int size = set.size();
+        System.out.println("총 객체 수: "+size);
+    }
+}
