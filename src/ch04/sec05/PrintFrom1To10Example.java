be2ca446ff50dc378db3c8043b93ed333d36cc2f
seo-faper <dswhdcks@gmail.com>
2023-01-10 11:35:46 +0900

    예제코드 : PrintFrom1To10Example.java

---
+++ src/ch04/sec05/PrintFrom1To10Example.java
@@ -0,0 +1,12 @@
+package ch04.sec05;
+
+public class PrintFrom1To10Example
+{
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <=10){
+            System.out.println(i+" ");
+            i++;
+        }
+    }
+}
