0760388f0169b02bb5a183fda056635229cb57d2
seo-faper <dswhdcks@gmail.com>
2023-01-10 18:55:21 +0900

    예제코드 : ArrayListExample.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 20;
-        System.out.println((++x)+(y--));
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
+       int myAccount = 0;
 
-        int n = sc.nextInt();
+        while (true){
+            System.out.println();
+           System.out.println("------------------------------------");
+           System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+           System.out.println("------------------------------------");
+           System.out.print("선택> ");
+           String userSelect = sc.nextLine();
+           if(userSelect.equals("1")){
+               System.out.print("예금액>");
+               myAccount += (Integer.parseInt(sc.nextLine()));
+           }else if(userSelect.equals("2")){
+               System.out.print("출금액>");
+               myAccount -= (Integer.parseInt(sc.nextLine()));
+
+           }else if(userSelect.equals("3")){
+               System.out.print("잔고>");
+               System.out.println(myAccount);
+
+           }else if(userSelect.equals("4")){
+               System.out.println("프로그램 종료");
+               break;
+           }
+        }
     }
 }
