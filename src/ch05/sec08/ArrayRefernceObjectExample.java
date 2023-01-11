08c6d8d6e24c0fb12812cfd1d601ed39f2b2b835
seo-faper <dswhdcks@gmail.com>
2023-01-11 12:57:33 +0900

    예제코드 : ArrayRefernceObjectExample.java

---
+++ src/ch05/sec08/ArrayRefernceObjectExample.java
@@ -0,0 +1,15 @@
+package ch05.sec08;
+
+public class ArrayRefernceObjectExample
+{
+    public static void main(String[] args) {
+        String[] strArray = new String[3];
+        strArray[0] = "Java";
+        strArray[1] = "Java";
+        strArray[2] = new String("Java");
+
+        System.out.println(strArray[0] == strArray[1]);
+        System.out.println(strArray[0] == strArray[2]);
+        System.out.println(strArray[0].equals(strArray[2]));
+    }
+}
