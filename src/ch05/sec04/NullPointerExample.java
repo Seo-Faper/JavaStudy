226791b1fd5189894998ff4521de106c61a8862b
seo-faper <dswhdcks@gmail.com>
2023-01-11 10:38:18 +0900

    예제코드 : NullPointerExample.java

---
+++ src/ch05/sec04/NullPointerExample.java
@@ -0,0 +1,14 @@
+package ch05.sec04;
+
+public class NullPointerExample
+{
+    public static void main(String[] args) {
+        int[] intArray = null;
+        //intArray[0] = 10;
+
+        String str = null;
+        //System.out.println(str);
+
+
+    }
+}
