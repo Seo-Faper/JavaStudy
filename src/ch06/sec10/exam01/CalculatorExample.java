03ac8e090f12ead97618994692f56b1e457d25bf
seo-faper <dswhdcks@gmail.com>
2023-01-12 14:15:46 +0900

    예제코드 : static 키워드

---
+++ src/ch06/sec10/exam01/CalculatorExample.java
@@ -0,0 +1,16 @@
+package ch06.sec10.exam01;
+
+public class CalculatorExample
+{
+    public static void main(String[] args) {
+        double result1 = 10*10*Calculator.pi;
+        int result2 = Calculator.plus(10,20);
+        int result3 = Calculator.minus(20,10);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+
+    }
+
+}
