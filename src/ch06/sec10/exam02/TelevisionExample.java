a4ccaf856e2702a087fac45b67d8405a37c29303
seo-faper <dswhdcks@gmail.com>
2023-01-12 14:21:51 +0900

    예제코드 : static 블록

--- src/ch06/sec10/exam02/TelevisionExample.java
+++ src/ch06/sec10/exam02/TelevisionExample.java
@@ -2,6 +2,6 @@ package ch06.sec10.exam02;
 
 public class TelevisionExample {
     public static void main(String[] args) {
-        System.out.println(Televison.info);
+        System.out.println( Televison.info);
     }
 }
