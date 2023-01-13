87d25c204e8290457a749ba68a2cca6b149e7389
seo-faper <dswhdcks@gmail.com>
2023-01-13 21:17:58 +0900

    예제코드 : 봉인된 메소드

---
+++ src/ch07/sec11/Person.java
@@ -0,0 +1,9 @@
+package ch07.sec11;
+
+public sealed class Person permits Employee,Manager {
+    public String name;
+    public void work(){
+        System.out.println("하는 일이 결정되지 않았습니다.");
+    }
+
+}
