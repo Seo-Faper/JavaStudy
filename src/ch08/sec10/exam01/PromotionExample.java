292e502e35b84d85dec5953f4e77d856857fe892
seo-faper <dswhdcks@gmail.com>
2023-01-16 22:05:56 +0900

    예제코드 : 인터페이스의 형변환

---
+++ src/ch08/sec10/exam01/PromotionExample.java
@@ -0,0 +1,11 @@
+package ch08.sec10.exam01;
+
+public class PromotionExample
+{
+    public static void main(String[] args) {
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        E e = new E();
+    }
+}
---
+++ src/ch08/sec10/exam02/CastingExample.java
@@ -0,0 +1,13 @@
+package ch08.sec10.exam02;
+
+public class CastingExample {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Bus();
+
+        vehicle.run();
+
+        Bus bus = (Bus) vehicle;
+        bus.run();
+        bus.checkFare();
+    }
+}
