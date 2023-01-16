ec81f3a2bd77e0e4bcaa2e50b250588229dac59a
seo-faper <dswhdcks@gmail.com>
2023-01-16 23:32:59 +0900

    예제코드 : 숙제

---
+++ src/ch08/homework/MysqlDao.java
@@ -0,0 +1,23 @@
+package ch08.homework;
+
+public class MysqlDao implements DataAccessObject{
+    @Override
+    public void select() {
+        System.out.println("MySql DB에서 검색");
+    }
+
+    @Override
+    public void insert() {
+        System.out.println("MySql DB에서 삽입");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("MySql DB를 수정");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("MySql DB에서 삭제");
+    }
+}
