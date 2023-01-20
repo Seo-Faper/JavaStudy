9fbb14a226e811033d8b212742ec92746a3a4663
seo-faper <dswhdcks@gmail.com>
2023-01-20 17:29:48 +0900

    예제코드 : 동전반즈부활사제 클래스

---
+++ src/ch12/sec05/StringTokenizerExample.java
@@ -0,0 +1,19 @@
+package ch12.sec05;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerExample {
+    public static void main(String[] args) {
+        String data1 = "홍딜동&이수홍,박연수";
+        String[] arr = data1.split("&|,");
+        for(String token : arr){
+            System.out.println(token);
+        }
+        String data2 = "홍길동/바언수/샌즈";
+        StringTokenizer st = new StringTokenizer(data2,"/");
+        while (st.hasMoreTokens()){
+            String token = st.nextToken();
+            System.out.println(token);
+        }
+    }
+}
