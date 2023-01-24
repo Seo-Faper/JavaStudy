35cdad6741b64371d5b4b19a52f8a769fe910896
seo-faper <dswhdcks@gmail.com>
2023-01-24 23:20:27 +0900

    예제코드 : 제네릭 메소드 신들린 연기

---
+++ src/ch13/sec03/exam01/Box.java
@@ -0,0 +1,13 @@
+package ch13.sec03.exam01;
+
+public class Box<T> {
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
