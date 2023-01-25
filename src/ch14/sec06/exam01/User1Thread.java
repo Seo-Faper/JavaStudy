2298e1fadc57a8b722ae289490bb606f040944d9
seo-faper <dswhdcks@gmail.com>
2023-01-26 03:15:55 +0900

    예제코드 : 동기화 스레드!!

--- src/ch14/sec06/exam01/User1Thread.java
+++ src/ch14/sec06/exam01/User1Thread.java
@@ -13,4 +13,9 @@ public class User1Thread extends Thread{
     public void setCalculator(Calculator calculator) {
         this.calculator = calculator;
     }
+
+    @Override
+    public void run() {
+        calculator.setMemory1(100);
+    }
 }
--- src/ch14/sec06/exam01/User2Thread.java
+++ src/ch14/sec06/exam01/User2Thread.java
@@ -13,4 +13,9 @@ public class User2Thread extends Thread{
     public void setCalculator(Calculator calculator) {
         this.calculator = calculator;
     }
+
+    @Override
+    public void run() {
+        calculator.setMemory2(50);
+    }
 }
