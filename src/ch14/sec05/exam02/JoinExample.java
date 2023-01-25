b11cf9dbbb7647fd9bbb87e46cfa20c6c3d921df
seo-faper <dswhdcks@gmail.com>
2023-01-26 03:03:46 +0900

    예제코드 : 멀티 스레드

---
+++ src/ch14/sec05/exam02/JoinExample.java
@@ -0,0 +1,12 @@
+package ch14.sec05.exam02;
+
+public class JoinExample {
+    public static void main(String[] args) {
+        SumThread sumThread = new SumThread();
+        sumThread.start();
+        try{
+            sumThread.join();
+        }catch (Exception e){}
+        System.out.println("1~100 합 : "+sumThread.getSum());
+    }
+}
