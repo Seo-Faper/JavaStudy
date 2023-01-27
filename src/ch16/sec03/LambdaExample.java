4be332575ec7446d88fb331aff3fdf7d0e8a2769
seo-faper <dswhdcks@gmail.com>
2023-01-27 17:14:36 +0900

    예제코드 : 매개변수가 있는 람다

---
+++ src/ch16/sec03/LambdaExample.java
@@ -0,0 +1,16 @@
+package ch16.sec03;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.action1((name, job) -> {
+            System.out.println(name+"이");
+            System.out.println(job+"을 합니다.");
+        });
+
+        person.action2(word->{
+            System.out.println(word+"라고 말합니다.");
+        });
+    }
+}
