ae0c919ac73b079dbd980c9c16cf0d091c575d65
seo-faper <dswhdcks@gmail.com>
2023-01-27 17:55:24 +0900

    예제코드 : 람다식의 메소드 참조

---
+++ src/ch16/sec05/exam01/MethodReferenceExample.java
@@ -0,0 +1,13 @@
+package ch16.sec05.exam01;
+
+public class MethodReferenceExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.action(Computer :: staticMethod);
+
+        Computer com = new Computer();
+
+        person.action(com::instanceMethod);
+    }
+}
