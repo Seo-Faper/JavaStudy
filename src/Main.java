e8f666b67d434ea36a2d23c13b0f0d5d712cd680
seo-faper <dswhdcks@gmail.com>
2023-01-30 02:21:48 +0900

    예제코드 : 생성자 참조

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        
-       Scanner sc = new Scanner(System.in);
-        int n = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] num = bf.readLine().split(" ");
+        int a = Integer.parseInt(num[0]);
+        int b = Integer.parseInt(num[1]);
+        int c = Integer.parseInt(num[2]);
 
-        int[] scores = null;
-        while (true){
-
-            System.out.println();
-           System.out.println("-------------------------------------------------------");
-           System.out.println("1. 학생 수 | 2. 점수입력 | 3. 점수리스트 | 4. 분석 |  5. 종료");
-           System.out.println("-------------------------------------------------------");
-           System.out.print("선택> ");
-           String userSelect = sc.nextLine();
-           if(userSelect.equals("1")){
-               System.out.print("학생수>");
-                n = sc.nextInt();
-                scores = new int[n];
-           }else if(userSelect.equals("2")){
-
-                for(int i = 0; i<n; i++){
-                    System.out.print("scores["+i+"]> ");
-                    scores[i] = sc.nextInt();
-                }
-
-           }else if(userSelect.equals("3")){
-               for(int i = 0; i<n; i++){
-                   System.out.println("scores["+i+"] : "+scores[i]);
-               }
-           }else if(userSelect.equals("4")){
-               System.out.println("분석");
-                int sum = 0;
-                int max = 0;
-                for(int i = 0; i<n; i++){
-                    sum +=scores[i];
-                    max = Math.max(scores[i],max);
-                }
-                System.out.println("최고 점수: "+max);
-                System.out.println("평균 점수: "+(double)sum/n);
-           }else if(userSelect.equals("5")){
-               System.out.println("프로그램 종료");
-               break;
-           }
-        }
+        if(c-b<=0) System.out.println( -1);
+        else System.out.println(a / (c-b)+1);
     }
 }
