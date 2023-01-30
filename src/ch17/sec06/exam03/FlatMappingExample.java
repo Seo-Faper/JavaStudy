a3cbfbb1258d195f22fcae8c2b42ab503117c72a
seo-faper <dswhdcks@gmail.com>
2023-01-30 20:22:11 +0900

    예제코드 : FlatMappingExample.java

--- src/ch17/sec06/exam03/FlatMappingExample.java
+++ src/ch17/sec06/exam03/FlatMappingExample.java
@@ -10,5 +10,17 @@ public class FlatMappingExample {
         list1.add("this is java");
         list1.add("i am a best developer");
         list1.stream().flatMap(data -> Arrays.stream(data.split(" "))).forEach(e->System.out.println(e));
+
+        System.out.println();
+
+        List<String> list2 = Arrays.asList("10","20","30","40","50");
+        list2.stream().flatMapToInt(e->{
+            String[] strArr = e.split(",");
+            int[] intArr = new int[strArr.length];
+            for(int i = 0; i<strArr.length; i++){
+                intArr[i] = Integer.parseInt(strArr[i].trim());
+            }
+            return Arrays.stream(intArr);
+        }).forEach(e->System.out.println(e));
     }
 }
