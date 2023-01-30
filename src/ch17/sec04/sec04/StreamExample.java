c796eaf32cf9b3cc20e1ef6b4aa1bc82521f72d9
seo-faper <dswhdcks@gmail.com>
2023-01-30 19:29:47 +0900

    예제코드 : 파일 스트림

--- src/ch17/sec04/sec04/StreamExample.java
+++ src/ch17/sec04/sec04/StreamExample.java
@@ -1,4 +1,18 @@
 package ch17.sec04.sec04;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class StreamExample {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        Path path = Paths.get(StreamExample.class.getResource("data.txt").toURI());
+        Stream<String> stream = Files.lines(path, Charset.defaultCharset());
+        stream.forEach(e->System.out.println(e));
+        stream.close();
+    }
 }
