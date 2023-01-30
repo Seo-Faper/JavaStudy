f685a8bde8c09e1cb22aa5e33d6d51b28df58344
seo-faper <dswhdcks@gmail.com>
2023-01-30 19:26:05 +0900

    예제코드 : 범위를 가지는 스트림

--- src/ch17/sec04/sec03/StreamExample.java
+++ src/ch17/sec04/sec03/StreamExample.java
@@ -1,4 +1,13 @@
 package ch17.sec04.sec03;
 
+import java.util.stream.IntStream;
+
 public class StreamExample {
+    public static int sum;
+
+    public static void main(String[] args) {
+        IntStream stream = IntStream.rangeClosed(1,100);
+        stream.forEach(e->sum +=e);
+        System.out.println(sum);
+    }
 }
---
+++ src/ch17/sec04/sec04/StreamExample.java
@@ -0,0 +1,4 @@
+package ch17.sec04.sec04;
+
+public class StreamExample {
+}
