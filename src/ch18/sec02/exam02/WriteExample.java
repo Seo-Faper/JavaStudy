0c100aa49c6bcf6d456ee145952a8a2c998605b7
seo-faper <dswhdcks@gmail.com>
2023-01-31 22:16:19 +0900

    예제코드 : WriteExample.java

--- src/ch18/sec02/exam02/WriteExample.java
+++ src/ch18/sec02/exam02/WriteExample.java
@@ -1,4 +1,4 @@
-package ch18.sec02.exam01;
+package ch18.sec02.exam02;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,21 +7,16 @@ import java.io.OutputStream;
 public class WriteExample {
     public static void main(String[] args) {
         try{
-            OutputStream os = new FileOutputStream("C:/Temp/test1.db");
+            OutputStream os = new FileOutputStream("C:/Temp/test2.db");
 
-            byte a = 10;
-            byte b = 20;
-            byte c = 30;
-
-            os.write(a);
-            os.write(b);
-            os.write(c);
+            byte[] array = {10,20,30};
+            os.write(array);
 
             os.flush();
             os.close();
 
         }catch(IOException e){
-
+            e.printStackTrace();
         }
 
     }
