5b1cc9844eb4699470303b3dfd7830d3b63a9439
seo-faper <dswhdcks@gmail.com>
2023-02-02 00:07:29 +0900

    예제코드 :  UDP NewsServer 동시 요청 처리

--- src/ch19/sec05/exam02/NewsServer.java
+++ src/ch19/sec05/exam02/NewsServer.java
@@ -1,8 +1,17 @@
 package ch19.sec05.exam02;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NewsServer extends Thread {
+    private static DatagramSocket datagramSocket = null;
+    private static ExecutorService executorService =
+            Executors.newFixedThreadPool(10);
     public static void main(String[] args) throws Exception {
         System.out.println("----------------------------------------");
         System.out.println("서버를 종료하려면 q를 입력하고 Enter 키를 입력하세요.");
@@ -18,5 +27,44 @@ public class NewsServer extends Thread {
 
         stopServer();
     }
+    public static void startServer(){
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    datagramSocket = new DatagramSocket(50001);
+                    System.out.println("[서버] 시작됨");
 
+                    while (true){
+                        DatagramPacket receivePacket = new DatagramPacket(
+                                new byte[1024],1024
+                        );
+                        datagramSocket.receive(receivePacket);
+                        executorService.execute(()->{
+                            try{
+                                String newsKind = new String(receivePacket.getData(),0,receivePacket.getLength(),"UTF-8");
+
+                                SocketAddress socketAddress =
+                                        receivePacket.getSocketAddress();
+
+                                for(int i = 1; i<=10; i++){
+                                    String data = newsKind+": 뉴스"+i;
+                                    byte[] bytes = data.getBytes("UTF-8");
+                                    DatagramPacket sendPack = new DatagramPacket(
+                                            bytes,0,bytes.length, socketAddress);
+                                }
+                            }catch(Exception e){}
+                        });
+                    }
+                }catch(Exception e){
+                    System.out.println("[서버] "+e.getMessage());
+                }
+            }
+        };
+    }
+    public static void stopServer(){
+        datagramSocket.close();
+        executorService.shutdownNow();
+        System.out.println("[서버] 종료됨.");
+    }
 }
