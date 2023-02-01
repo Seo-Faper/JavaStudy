9e90688bd03afd450f52b6dac3533bc90b9832a1
seo-faper <dswhdcks@gmail.com>
2023-02-01 21:49:04 +0900

    예제코드 :  네트워킹

---
+++ src/ch19/sec03/exam01/ServerExample.java
@@ -0,0 +1,65 @@
+package ch19.sec03.exam01;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerExample {
+
+    private static ServerSocket serverSocket = null;
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------------");
+        System.out.println("서버를 종료하려면 q 또는 Q를 입력하고 Enter 키를 입력하세요.");
+        System.out.println("----------------------------------------------------");
+        
+        startServer();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String key = scanner.nextLine();
+            if(key.toLowerCase().equals("q")) break;
+        }
+        
+        scanner.close();
+        
+        stopServer();
+        
+    }
+
+
+    public static void startServer() {
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    serverSocket = new ServerSocket(50001);
+                    System.out.println("[서버] 시작됨");
+
+                    while (true){
+                        System.out.println("\n[서버] 연결 요청을 기다림\n");
+                        Socket socket = serverSocket.accept();
+
+                        InetSocketAddress isa =
+                                (InetSocketAddress) socket.getRemoteSocketAddress();
+                        System.out.println("[서버]"+isa.getHostName()+"의 연결 요청을 수락함");
+
+                        socket.close();
+                        System.out.println("[서버] "+isa.getHostName()+"의연결을 끊음");
+                    }
+                }catch(IOException e){
+                        System.out.println("[서버] "+e.getMessage());
+                }
+            }
+        };
+        thread.start();
+    }
+    private static void stopServer() {
+        try{
+            serverSocket.close();
+            System.out.println("[서버] 종료됨.");
+        }catch (IOException e){}
+    }
+
+}
