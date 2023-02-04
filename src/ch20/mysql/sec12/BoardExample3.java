0ed95f356394a44e3ef464bf2d6379741094f0c5
seo-faper <dswhdcks@gmail.com>
2023-02-04 21:42:49 +0900

    예제코드 : 게시글 페이지

--- src/ch20/mysql/sec12/BoardExample3.java
+++ src/ch20/mysql/sec12/BoardExample3.java
@@ -70,23 +70,157 @@ public class BoardExample3 {
         }
     }
 
-    private void exit() {
-        System.out.println("***exit() 실행됨***");
+    private void clear() {
+        System.out.println("[게시물 전체 삭제]");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("보조 메뉴: 1.Ok | 2.Cancel");
+        System.out.print("메뉴 선택: ");
+        String menuNo = sc.nextLine();
+        if(menuNo.equals("1")) {
+             try {
+                 String sql = "TRUNCATE TABLE boards";
+                 PreparedStatement pstmt = conn.prepareStatement(sql);
+                 pstmt.executeUpdate();
+                 pstmt.close();
+                 } catch (Exception e) {
+                 e.printStackTrace();
+                 exit();
+                 }
+             }
         list();
     }
 
-    private void clear() {
-        System.out.println("***clear() 실행됨***");
-        list();
+    private void exit() {
+        if(conn != null){
+            try{
+                conn.close();
+            }catch(SQLException e){
+
+            }
+        }
+        System.out.println("** 게시판 종료 **");
+        System.exit(0);
     }
 
     private void read() {
-        System.out.println("***read() 실행됨***");
+        System.out.println("[게시물 읽기]");
+        System.out.print("bno: ");
+        int bno = Integer.parseInt(sc.nextLine());
+        try{
+            String sql = "" +
+                     "SELECT bno, btitle, bcontent, bwriter, bdate " +
+                     "FROM boards " +
+                     "WHERE bno= ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, bno);
+             ResultSet rs = pstmt.executeQuery();
+            if(rs.next()) {
+                 Board board = new Board();
+                 board.setBno(rs.getInt("bno"));
+                 board.setBtitle(rs.getString("btitle"));
+                 board.setBcontent(rs.getString("bcontent"));
+                 board.setBwriter(rs.getString("bwriter"));
+                 board.setBdate(rs.getDate("bdate"));
+                 System.out.println("#############");
+                 System.out.println("번호: " + board.getBno());
+                 System.out.println("제목: " + board.getBtitle());
+                 System.out.println("내용: " + board.getBcontent());
+                 System.out.println("작성자: " + board.getBwriter());
+                 System.out.println("날짜: " + board.getBdate());
+                 System.out.println("#############");
+                 System.out.println("----------------------");
+                 System.out.println("보조 메뉴 : 1.Update | 2.Delete | 3.List");
+                 System.out.print("메뉴 선택");
+                 String menuNo = sc.nextLine();
+                if (menuNo.equals("1")) {
+                    update(board);
+                }else if(menuNo.equals("2")){
+                    delete(board);
+                }else{
+
+                }
+            }
+             rs.close();
+             pstmt.close();
+            }    catch(Exception e){}
         list();
     }
+    private void delete(Board board){
+        try{
+            String sql = "DELETE FROM boards WHERE bno=?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1,board.getBno());
+            pstmt.executeUpdate();
+            pstmt.close();
+        }catch(Exception e){
+            exit();
+        }
+        list();
+    }
+    private void update(Board board) {
 
+        System.out.println("[수정 내용 입력]");
+         System.out.print("제목: ");
+         board.setBtitle(sc.nextLine());
+         System.out.print("내용: ");
+         board.setBcontent(sc.nextLine());
+         System.out.print("작성자: ");
+         board.setBwriter(sc.nextLine());
+
+         //보조 메뉴 출력
+         System.out.println("------------------------------------------------------");
+         System.out.println("보조 메뉴: 1.Ok | 2.Cancel");
+         System.out.print("메뉴 선택: ");
+         String menuNo = sc.nextLine();
+         if(menuNo.equals("1")) {
+
+            try {
+                String sql = "" +
+                         "UPDATE boards SET btitle= ?, bcontent= ?, bwriter= ? " +
+                         "WHERE bno= ?";
+                 PreparedStatement pstmt = conn.prepareStatement(sql);
+                 pstmt.setString(1, board.getBtitle());
+                 pstmt.setString(2, board.getBcontent());
+                 pstmt.setString(3, board.getBwriter());
+                 pstmt.setInt(4, board.getBno());
+                 pstmt.executeUpdate();
+                 pstmt.close();
+            } catch (Exception e) {
+                 e.printStackTrace();
+                 exit();
+                 }
+             }
+         list();
+    }
     private void create() {
-        System.out.println("***create() 실행됨***");
+        Board board = new Board();
+        System.out.println("[새 게시물 입력]");
+        System.out.print("제목: ");
+        board.setBtitle(sc.nextLine());
+        System.out.print("내용: ");
+        board.setBcontent(sc.nextLine());
+        System.out.print("작성자: ");
+        board.setBwriter(sc.nextLine());
+
+        System.out.println("------------------------------------------------------");
+        System.out.println("보조 메뉴 : 1. OK | 2. Cancel");
+        System.out.print("메뉴 선택 : ");
+        String menuNo = sc.nextLine();
+        if(menuNo.equals("1")){
+            try{
+                String sql = ""+
+                        "INSERT INTO boards (btitle, bcontent, bwriter, bdate) " +
+                        "VALUES (?, ?, ?, now())";
+                PreparedStatement psmt = conn.prepareStatement(sql);
+                psmt.setString(1,board.getBtitle());
+                psmt.setString(2,board.getBcontent());
+                psmt.setString(3,board.getBwriter());
+                psmt.executeUpdate();
+                psmt.close();
+            }catch(Exception e){
+
+            }
+        }
         list();
     }
 
